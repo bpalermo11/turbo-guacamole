b75c8b75ee013530e2df24c3aa9fcb6520d288ed
Bob Palermo <bpalermo11@gmail.com>
2022-04-08 12:45:11 -0400

    initial commit

---
+++ DemoUtilities/TurboFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TurboUtilities
+{
+    public class StringFunctions
+    {
+        public string Reverse(string s)
+        {
+            char[] charArray = s.ToUpper().ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
